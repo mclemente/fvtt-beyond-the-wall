92b03145759fecd956efbf2c06e1d967899585bb
Matheus Clemente <matheus.xb@gmail.com>
2025-06-24 03:16:53 -0300

    Add Currencies

--- src/module/data/actor/character.js
+++ src/module/data/actor/character.js
@@ -7,6 +7,29 @@ export default class CharacterData extends ActorTemplateData {
 		return {
 			...superFields,
 			background: new fields.HTMLField(),
+			currency: new fields.SchemaField({
+				gold: new fields.NumberField({
+					required: true,
+					nullable: false,
+					integer: true,
+					initial: 0,
+					min: 0
+				}),
+				silver: new fields.NumberField({
+					required: true,
+					nullable: false,
+					integer: true,
+					initial: 0,
+					min: 0
+				}),
+				copper: new fields.NumberField({
+					required: true,
+					nullable: false,
+					integer: true,
+					initial: 0,
+					min: 0
+				})
+			}),
 			fortunePoints: new fields.SchemaField({
 				value: new fields.NumberField({
 					required: true, nullable: false, integer: true, min: 0, initial: 3
@@ -15,7 +38,7 @@ export default class CharacterData extends ActorTemplateData {
 					required: true, nullable: false, integer: true, min: 0, initial: 3
 				})
 			}),
-			level: new fields.NumberField({ nullable: false, integer: true, min: 0, initial: 0 }),
+			level: new fields.NumberField({ nullable: false, integer: true, min: 0, initial: 0 })
 		};
 	}
 }
